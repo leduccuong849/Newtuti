05692bc4da76d557ad55b12fb618cffba82e3fb0
leduccuong <leduccuongwru@gmail.com>
2023-06-02 19:54:01 +0700

    hello

---
+++ ProjectNew/IOSnangcao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSnangcao
+//
+//  Created by Cường Lê Đức on 31/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectNew/IOSnangcao/itemSectionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  itemSectionCollectionViewCell.swift
+//  IOSnangcao
+//
+//  Created by Cường Lê Đức on 31/05/2023.
+//
+
+import UIKit
+
+class itemSectionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
